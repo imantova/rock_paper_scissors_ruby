31058f79c6f4e90a014109a153ef6c3c747eef12
imantova <Irene@wireless-s0-no-150-6-40.uchicago.edu>
2015-10-09 11:10:57 -0500

    work on reps

--- rps.rb
+++ rps.rb
@@ -1,1 +1,37 @@
 # Rock, Paper, Scissors
+puts "your move: rock, paper or scissors?"
+
+move_user = gets.chomp
+
+puts "you chose #{move_user}."
+
+computer_move = ["rock","paper","scissors"].sample
+
+puts "the computer chose #{computer_move}."
+
+if computer_move == move_user
+    puts "it's a tie"
+
+elsif computer_move == "paper" && move_user == "rock"
+    puts "you win!"
+
+elsif computer_move == "scissors" && move_user == "rock"
+    puts "you win!"
+
+elsif computer_move == "rock" && move_user == "paper"
+	puts "you lose!"
+
+elsif computer_move == "scissors" && move_user == "paper"
+	puts "you lose!"
+
+elsif computer_move == "rock" && move_user == "scissors"
+	puts "you win!"
+
+elsif computer_move == "paper" && move_user == "scissors"
+	puts "you lose!"
+end
+
+	
+	
+
+
